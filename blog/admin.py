170a964e321ecc1a91270db44a4fe79dfe7d72ad
lmt <981433814@qq.com>
2019-07-24 19:39:37 +0800

    第四次修改’

--- blog/admin.py
+++ blog/admin.py
@@ -25,19 +25,34 @@ class TagAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
-        'title', 'category', 'status', 'created_time', 'operator'
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
     ]
     list_display_links = []
 
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
     actions_on_top = True
     actions_on_bottom = True
     #编辑页面
-    save_on_top = True
+    save_on_top = False
 
     fields = (
         ('category', 'title'),
@@ -46,7 +61,6 @@ class PostAdmin(admin.ModelAdmin):
         'content',
         'tag',
     )
-
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
@@ -56,3 +70,6 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
--- config/admin.py
+++ config/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Link, SideBar
 # Register your models here.
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'href', 'status', 'weight', 'created_time')
+    fields = ('title', 'href', 'status', 'weight')
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(LinkAdmin, self).save_model(request, obj, form, change)
+@admin.register(SideBar)
+class SideBarAdim(admin.ModelAdmin):
+    list_display = ('title', 'display_type', 'content', 'created_time')
+    list_filter = ('title', 'display_type', 'content')
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(SideBar, self).save_model(request, obj, form, change)
